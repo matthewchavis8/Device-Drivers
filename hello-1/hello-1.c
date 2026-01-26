4f6b3e798d5091e0debfe077e9795396deca16b1
matthewchavis8 <matthewchavis8>
2026-01-26 14:48:21 -0500

    Fixed conflict in hello

--- hello-1/hello-1.c
+++ hello-1/hello-1.c
@@ -3,18 +3,16 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-
 static int hello_init(void) {
-  printk(KERN_ALERT "Hello WORLD I AM ALIVE\n");
+  pr_debug("Hello World!\n");
   return 0;
 }
 
 static void hello_exit(void) {
-  printk(KERN_ALERT "Goodbye world \n");
+  pr_debug("Goodbye World!\n");
 }
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("This is my first Module for hello world");
-
 module_init(hello_init);
 module_exit(hello_exit);
