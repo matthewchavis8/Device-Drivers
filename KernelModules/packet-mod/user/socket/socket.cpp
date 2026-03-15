d5ae167115e6f892d654c5c5e69ccfe8f809ddaa
matthewchavis8 <matthewchavis8>
2026-03-15 00:13:56 -0400

    user socket commetns

--- KernelModules/packet-mod/user/socket/socket.cpp
+++ KernelModules/packet-mod/user/socket/socket.cpp
@@ -7,18 +7,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-/*sockaddr_in addr{};*/
-/*addr.sin_family = AF_INET;*/
-/*addr.sin_port = htons(port);*/
-/**/
-/*if (::inet_pton(AF_INET, ip.data(), &addr.sin_addr) != 1) {*/
-/*    throw std::runtime_error("inet_pton() failed");*/
-/*}*/
-/**/
-/*if (::connect(fd_, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) < 0) {*/
-/*    throw std::runtime_error(std::strerror(errno));*/
-/*}*/
-
 Socket::Socket(): m_fd {::socket(AF_INET, SOCK_STREAM, 0)}
 {
   if (m_fd < 0)
@@ -30,17 +18,20 @@ void Socket::connect(std::string_view ip, uint16_t port) const {
   addr.sin_family = AF_INET;
   addr.sin_port   = htons(port);
 
-  if (::inet_pton(AF_INET, ip.data(), &addr.sin_addr) != 1)
+  // Convert IP address into a binary network representation and write into addr
+  int addr_pton = ::inet_pton(AF_INET, ip.data(), &addr.sin_addr) != 1;
+  if (addr_pton != 1)
     throw std::runtime_error("[ERROR] inet_pton() failed\n");
 
-  if (::connect(m_fd, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) < 0)
-    throw std::runtime_error("[ERROR] connect failed\n");
+  int tcp_connect = ::connect(m_fd, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr));
+  if (tcp_connect < 1)
+    throw std::runtime_error("[ERROR] connection failed\n");
 }
 
 void Socket::send(std::string_view msg) const {
   const ssize_t res = ::send(m_fd, msg.data(), msg.size(), 0);
   if (res < 0)
-    throw std::runtime_error("[ERROR] connect failed\n");
+    throw std::runtime_error("[ERROR] failed to send message\n");
 }
 
 Socket::~Socket() {
